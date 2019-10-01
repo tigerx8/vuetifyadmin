eca4d37afac8164d50291613870456ca99f82b67
tigerx8 <blowupyours@gmail.com>
2019-10-01 22:29:23 +0700

    Init App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Vuetify</span>
+				<span class="font-weight-light">MATERIAL DESIGN</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+				<span class="mr-2">Latest Release</span>
+			</v-btn>
+		</v-app-bar>
+
+		<v-content>
+			<Youtube />
+		</v-content>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld";
+import Youtube from "./components/Youtube";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Youtube
+	},
+	data: () => ({
+		//
+	})
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+// import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+// import VueI18n from 'vue-i18n';
+
+Vue.use(Vuetify);
+// Vue.use(VueI18n);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+    },
+});
